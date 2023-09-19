e7f8b73b481737f409d3db7002933bb0024254a4
Jennifer Chiou <jennifer.chiou@mail.utoronto.ca>
2023-09-18 20:00:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Shows/hides nav-bar menu for mobile users
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
